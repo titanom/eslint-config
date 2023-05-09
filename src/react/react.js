9751e216483570b13354f429dada3da14cda9fc1
Jannis Morgenstern <nyarthan@proton.me>
2023-05-09 22:21:51 +0200

    chore: format

--- src/react/react.js
+++ src/react/react.js
@@ -142,10 +142,7 @@ module.exports = {
         'react/jsx-filename-extension': [ERROR, { extensions: ['.jsx', '.tsx'] }],
         'react/jsx-first-prop-new-line': [WARN, 'multiline'],
         'react/jsx-fragments': [ERROR, 'element'],
-        'react/jsx-handler-names': [
-          OFF,
-          
-        ],
+        'react/jsx-handler-names': [OFF],
         'react/jsx-indent': [WARN, 2, { checkAttributes: true, indentLogicalExpressions: true }],
         'react/jsx-indent-props': [WARN, 2],
         'react/jsx-key': ERROR,
@@ -160,7 +157,7 @@ module.exports = {
         'react/jsx-no-script-url': ERROR,
         'react/jsx-no-target-blank': ERROR,
         'react/jsx-no-undef': ERROR,
-        'react/jsx-no-useless-fragment': [ERROR, {allowExpressions: true}],
+        'react/jsx-no-useless-fragment': [ERROR, { allowExpressions: true }],
         'react/jsx-one-expression-per-line': OFF,
         'react/jsx-pascal-case': ERROR,
         'react/jsx-props-no-multi-spaces': WARN,
